692cc01361567ba057181f9661969c1d517b41a2
Michel Kreutner <mkreutner@gmail.com>
2025-09-06 07:47:08 +0200

    chore: add restore script

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 ##############################################################################
-# Desc: Restore script form source folder to destination folder
+# Desc: Restore script form archive into destination folder
 ##############################################################################
 
 # Check
-if [ $# -ne 2 ]; then
-    echo "Please, give FROM and TO directories: $0 /full/path/FROM/without/trailing/slash /full/path/TO/without/trailing/slash"
+if [ $# -ne 1 ]; then
+    echo "Please, give me what to restore! "
+    echo "Usage: $0 <archive_name>"
     exit 1
 fi
+
+# Where to extract
+day=$(date +%Y%m%d)
+hostname=$(hostname -s)
+username=$(id -u -n)
+archname="${1}"
+tmpdir="${HOME}/backup/tmp/"
+
+if [ ! -d $tmpdir ]; then
+    echo "ℹ️  - Create temporary directory"
+    mkdir -p $tmpdir
+fi
+
+# Restore archive
+tar -xzvf ${archname} -C ${tmpdir}
